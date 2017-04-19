c64636845db03bb5cd7ca9c87647f50bd6978c0e
Grey-Echo <hugues.bousquet@outlook.fr>
2017-04-19 12:05:05 +0200

    fix for chrome

--- source/javascripts/all_nosearch.js
+++ source/javascripts/all_nosearch.js
@@ -2,14 +2,14 @@
 //= require ./app/_toc
 //= require ./app/_lang
 
-$(function() {
+setTimeout(function() {
   loadToc($('#toc'), '.toc-link', '.toc-list-h2, .toc-list-h3', 10);
   setupLanguages($('body').data('languages'));
   $('.content').imagesLoaded( function() {
     window.recacheHeights();
     window.refreshToc();
   });
-});
+}, 3000);
 
 window.onpopstate = function() {
   activateLanguage(getLanguageFromQueryString());
